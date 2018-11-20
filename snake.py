35661fdb563fcd5be8eefa5797b25e99d57234d2
XenonLab-Studio <xenonlab.develop@gmail.com>
2018-11-20 13:19:17 +0100

    #2 Addition of the body class and the food class

--- snake.py
+++ snake.py
@@ -25,17 +25,15 @@ import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
 from random import randint
 
-# Define some variables
-WIDTH = 35 # total game cell width and height
+WIDTH = 35
 HEIGHT = 20
-MAX_X = WIDTH - 2 # sets max inside the game, so the snake wont hit the border
+MAX_X = WIDTH - 2
 MAX_Y = HEIGHT - 2
-SNAKE_LENGTH = 5 # Starting snake length
+SNAKE_LENGTH = 5
 SNAKE_X = SNAKE_LENGTH + 1
 SNAKE_Y = 3
-TIMEOUT = 100 # this controls the speed of the game
+TIMEOUT = 100
 
-# Make Snake Oject
 class Snake(object):
     def __init__(self):
         self.x = 'Hisss!'
@@ -45,3 +43,23 @@ class Snake(object):
 
 snake = Snake() # We do not pass any argument to the __init__ method
 snake.method_a('Says the snake') # We only pass a single argument
+
+# Make Body Oject
+class Body(object):
+    def __init__(self):
+        self.x = 'this is the'
+
+    def method_a(self, foo):
+        print self.x + ' ' + foo
+
+body = Body().method_a('Body')
+
+# Make Food Oject
+class Food(object):
+    def __init__(self):
+        self.y = 'Yum, Tasty'
+
+    def method_a(self, foo):
+        print self.y + ' ' + foo
+
+food = Food().method_a('Food')
